026fa619a90e2ca6841f61db0f97c3d4fe058bea
Muhammed Iqbal <iquzart@hotmail.com>
2023-05-10 16:49:21 +0400

    added comments

--- routes/systemRoutes.go
+++ routes/systemRoutes.go
@@ -1,3 +1,4 @@
+// Package routes contains the system routes, which map user requests to the appropriate controller and method.
 package routes
 
 import (
@@ -7,8 +8,14 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+// addSystemRoutes adds the routes for the System controller to the specified router group.
 func addSystemRoutes(rg *gin.RouterGroup) {
+	// Create a new group of routes for the System controller under the specified router group.
 	system := rg.Group("/system")
+
+	// Add a GET route for the Health method of the System controller.
 	system.GET("/health", controllers.Health)
+
+	// Add a GET route for the Metrics endpoint using the Prometheus HTTP handler.
 	system.GET("/metrics", gin.WrapH(promhttp.Handler()))
 }
