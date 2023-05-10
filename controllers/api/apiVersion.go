143e477ba08792f74d3f2fed0504d75ab702cea5
Muhammed Iqbal <iquzart@hotmail.com>
2023-05-10 16:49:41 +0400

    added comments

--- controllers/api/apiVersion.go
+++ controllers/api/apiVersion.go
@@ -1,3 +1,4 @@
+// APIVersion is a controller method that returns the API version.
 package controllers
 
 import (
@@ -8,11 +9,13 @@ import (
 )
 
 func APIVersion(ctx *gin.Context) {
+	// Get the API version from the "API_VERSION" environment variable, or use a default value of "v1.0.0" if not set.
 	apiVersion := os.Getenv("API_VERSION")
 	if apiVersion == "" {
 		apiVersion = "v1.0.0"
 	}
 
+	// Respond with a 200 status code and the API version as the response body.
 	ctx.String(
 		http.StatusOK,
 		apiVersion,
